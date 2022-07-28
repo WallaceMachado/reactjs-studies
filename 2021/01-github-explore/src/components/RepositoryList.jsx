24d8acb48aa14c3fc4ae712803fdde7879bdb674
wallace machado <wallacemachado@ntopus.com>
2022-07-28 14:56:21 -0300

    propriedades no React

--- 2021/01-github-explore/src/components/RepositoryList.jsx
+++ 2021/01-github-explore/src/components/RepositoryList.jsx
@@ -1,36 +1,24 @@
-const repositoryName = 'unform2'
+import { RepositoryItem } from "./RepositoryItem"
 
+const repository= {  
+      name: 'unform',
+      description: 'forms in react',
+      html_url: 'https://github.com/unform/unform',
+    
+  }
+// repositoryList é pai de RepositoryItem e sendo assim pode passar uma propriedade um valor pelo props
+// para utilizarmos js com html inserimos a {} e colocamos o codigo js dentro
+// por padrão nomeamos um componente com letra maiscula e apenas um coponente por aquivo
 export function RepositoryList(){
     return (
         <section className="repository-list">
             <h1>Lista de repositorios</h1>
 
             <ul>
-                <li>
-                    <strong>{repositoryName}</strong>
-                    <p>Forms in React</p>
-                    <a href="">
-                        Acessar repositorio
-                    </a>
-                </li>
-            </ul>
-            <ul>
-                <li>
-                    <strong>unform</strong>
-                    <p>Forms in React</p>
-                    <a href="">
-                        Acessar repositorio
-                    </a>
-                </li>
-            </ul>
-            <ul>
-                <li>
-                    <strong>unform</strong>
-                    <p>Forms in React</p>
-                    <a href="">
-                        Acessar repositorio
-                    </a>
-                </li>
+                <RepositoryItem repository={repository}/>
+                <RepositoryItem repository={repository}/>
+                <RepositoryItem repository={repository}/>
+                <RepositoryItem repository={repository}/>
             </ul>
         </section>
     )
